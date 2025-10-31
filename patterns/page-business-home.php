f2734cd5c68e8dd913d4c460b40f9f69bea3daf3
parth486 <parikhparth11@gmail.com>
2025-10-31 14:07:05 +0530

    Uploading Basic custom FSE theme created for Urbana Designs

---
+++ patterns/page-business-home.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: Business homepage
+ * Slug: urbana/page-business-home
+ * Categories: urbana_page, featured
+ * Keywords: starter
+ * Block Types: core/post-content
+ * Post Types: page, wp_template
+ * Viewport width: 1400
+ * Description: A business homepage pattern.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Urbana 1.0
+ */
+
+?>
+
+<!-- wp:pattern {"slug":"urbana/cta-centered-heading"} /-->
+<!-- wp:pattern {"slug":"urbana/overlapped-images"} /-->
+<!-- wp:pattern {"slug":"urbana/services-3-col"} /-->
+<!-- wp:pattern {"slug":"urbana/testimonials-large"} /-->
+<!-- wp:pattern {"slug":"urbana/pricing-2-col"} /-->
+<!-- wp:pattern {"slug":"urbana/cta-newsletter"} /-->
---
+++ patterns/page-landing-podcast.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: Landing page for podcast
+ * Slug: urbana/page-landing-podcast
+ * Categories: urbana_page, featured
+ * Keywords: starter
+ * Block Types: core/post-content
+ * Post Types: page, wp_template
+ * Viewport width: 1400
+ * Description: A landing page for the podcast with a hero section, description, logos, grid with videos and newsletter signup.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Urbana 1.0
+ */
+
+?>
+
+<!-- wp:pattern {"slug":"urbana/hero-podcast"} /-->
+<!-- wp:pattern {"slug":"urbana/heading-and-paragraph-with-image"} /-->
+<!-- wp:pattern {"slug":"urbana/logos"} /-->
+<!-- wp:pattern {"slug":"urbana/grid-videos"} /-->
+<!-- wp:pattern {"slug":"urbana/cta-newsletter"} /-->
